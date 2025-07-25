149fceb07935d5661085a21484ccaf5c5295cb76
Prikossoski <priscilag_pg@yahoo.com.br>
2025-07-25 18:06:37 +0000

    NIVEL MESTRE

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,82 +1,103 @@
 #include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
-#define NAVIO_10 3
-#define VALOR_NAVIO 1
+#define TAM 10           // Tamanho do tabuleiro
+#define NAVIO_TAM 3      // Tamanho dos navios
+#define VALOR_NAVIO 3    // Valor para representar um navio
+#define VALOR_HABILIDADE 5 // Valor para área afetada pela habilidade
 
-void exibirTabuleiro(int tabuleiro[10][10]) {
-    char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
+// Função para aplicar uma matriz de habilidade no tabuleiro
+void aplicarHabilidade(int tabuleiro[TAM][TAM], int habilidade[5][5], int origemLinha, int origemColuna) {
+    int tamanho = 5;
+    int meio = tamanho / 2;
 
-    printf("   ");
-    for (int j = 0; j < 10; j++) {
-    printf("%2d ", j);
+    for (int i = 0; i < tamanho; i++) {
+    for (int j = 0; j < tamanho; j++) {
+    if (habilidade[i][j] == 1) {
+    int linhaTab = origemLinha + (i - meio);
+    int colunaTab = origemColuna + (j - meio);
     }
-    printf("\n");
-
-    for (int i = 0; i < 10; i++) {
-    printf("%2c ", linha[i]);
-    for (int j = 0; j < 10; j++) {
-     if (tabuleiro[i][j] == VALOR_NAVIO)
-      printf(" 3 ");  // Navio
-      else
-      printf(" ~ ");  // Água
-     }
-    printf("\n");
+    
+    // Verifica se está dentro dos limites do tabuleiro
+    
+    if (linhaTab >= 0 && linhaTab < TAM && colunaTab >= 0 && colunaTab < TAM) {
+    if (tabuleiro[linhaTab][colunaTab] == 0) {
+    tabuleiro[linhaTab][colunaTab] = VALOR_HABILIDADE;
+    }
+    }
     }
     }
 
-     int main() {
+int main() {
+    
+    int tabuleiro[TAM][TAM];
 
-      int tabuleiro[10][10];
-
-        // Inicializa o tabuleiro com água (0)
-
-        for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    // Inicializa o tabuleiro com água (0)
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
             tabuleiro[i][j] = 0;
         }
-        }
+    }
 
-        // Posições iniciais dos navios
-       int linhaH = 2, colunaH = 4;  // horizontal
-       int linhaV = 6, colunaV = 7;  // vertical
-       int linhaD1 = 0, colunaD1 = 0; // diagonal principal
-       int linhaD2 = 0, colunaD2 = 7; // diagonal secundária
+    // Define posições dos navios
+    int linhaH = 2, colunaH = 4;
+    int linhaV = 6, colunaV = 7;
 
-       // Verificações para garantir que os navios cabem no tabuleiro
-       if (
-        colunaH + NAVIO_10 <= 10 &&
-        linhaV + NAVIO_10 <= 10 &&
-        linhaD1 + NAVIO_10 <= 10 && colunaD1 + NAVIO_10 <= 10 &&
-        linhaD2 + NAVIO_10 <= 10 && colunaD2 - (NAVIO_10 - 1) >= 0
-        ) {
+    if (colunaH + NAVIO_TAM <= TAM && linhaV + NAVIO_TAM <= TAM) {
         // Navio horizontal
-        for (int i = 0; i < NAVIO_10; i++) {
+        for (int i = 0; i < NAVIO_TAM; i++) {
             tabuleiro[linhaH][colunaH + i] = VALOR_NAVIO;
         }
 
         // Navio vertical
-        for (int i = 0; i < NAVIO_10; i++) {
+        for (int i = 0; i < NAVIO_TAM; i++) {
             tabuleiro[linhaV + i][colunaV] = VALOR_NAVIO;
         }
-
-        // Navio diagonal principal (↘)
-        for (int i = 0; i < NAVIO_10; i++) {
-            tabuleiro[linhaD1 + i][colunaD1 + i] = VALOR_NAVIO;
-        }
-
-        // Navio diagonal secundária (↙)
-        for (int i = 0; i < NAVIO_10; i++) {
-            tabuleiro[linhaD2 + i][colunaD2 - i] = VALOR_NAVIO;
-        }
-
-        // Exibe o tabuleiro final com os 4 navios
-        exibirTabuleiro(tabuleiro);
-
-       } else {
-        printf("Erro: Um ou mais navios não cabem no tabuleiro.\n");
+    } else {
+        printf("Erro: os navios não cabem no tabuleiro.\n");
         return 1;
-       }
+    }
+
+    // Matriz de habilidade - Cone
+    int cone[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    // Matriz de habilidade - Cruz
+    int cruz[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0}
+    };
+
+    // Matriz de habilidade - Octaedro
+    int octaedro[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0}
+    };
+
+    // Aplica as habilidades no tabuleiro
+    aplicarHabilidade(tabuleiro, cone, 1, 1);       // Ponto de origem para cone
+    aplicarHabilidade(tabuleiro, cruz, 5, 5);       // Ponto de origem para cruz
+    aplicarHabilidade(tabuleiro, octaedro, 8, 2);   // Ponto de origem para octaedro
+
+    // Exibe o tabuleiro
+    printf("Tabuleiro:\n");
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
-     }
+}
